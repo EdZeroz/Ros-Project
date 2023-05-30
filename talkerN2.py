25b7f2845a372827206ed6b8515c8eb2cc7dabbb
EdZero <129580772+EdZeroz@users.noreply.github.com>
2023-05-30 17:14:35 +0700

    Update talkerN2.py

--- talkerN2.py
+++ talkerN2.py
@@ -6,7 +6,7 @@ def talker():
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "Ros Project Jakkrit"
+        hello_str = "Ros Project Jakkrit From pub2 Karb"
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
