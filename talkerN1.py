7a7efb767e7ca812f68a4e3c963d05a8b511850b
EdZero <129580772+EdZeroz@users.noreply.github.com>
2023-05-30 17:12:41 +0700

    Update talkerN1.py

--- talkerN1.py
+++ talkerN1.py
@@ -6,7 +6,7 @@ def talker():
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "Hi I am Mick from pub1"
+        hello_str = "Sawasdeekarb I am Mick from pub1"
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
